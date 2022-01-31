c13cff0c430518f8fb914961dcabea3273719e31
Larissa Wong <62777859+Larilw@users.noreply.github.com>
2022-01-31 00:14:53 -0300

    Versão final
    Adição de comentários no código
    Geração dos arquivos de teste fonte1 e fonte2
    Atualização do README

--- Lex.java
+++ Lex.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
  
+/*
+Descricao: Classe que realiza a análise léxica de um arquivo
+*/
 public class Lex {
-    private int line;
+    private int linha;
     private int pos;
     private int position;
     private char chr;
@@ -13,17 +16,26 @@ public class Lex {
  
     Map<String, TokenType> keywords = new HashMap<>();
  
+    /*
+    Descricao: Classe que representa um token 
+    */
     static class Token {
         public TokenType tipotoken;
         public String valor;
-        public int line;
+        public int linha;
         public int pos;
         Token(TokenType token, String valor, int linha, int pos) {
-            this.tipotoken = token; this.valor = valor; this.line = linha; this.pos = pos;
+            this.tipotoken = token; this.valor = valor; this.linha = linha; this.pos = pos;
         }
+
+        /*
+        Descricao: Formata a função toString para apresentar o token da maneira desejada de acordo com sua classe
+        Entrada: Nenhuma
+        Retorno: String formatada
+        */
         @Override
         public String toString() {
-            String result = String.format("%5d  %5d %-15s", this.line, this.pos, this.tipotoken);
+            String result = String.format("%5d  %5d %-15s", this.linha, this.pos, this.tipotoken);
             switch (this.tipotoken) {
                 case Inteiro:
                     result += String.format("  %4s", valor);
@@ -39,26 +51,38 @@ public class Lex {
         }
     }
  
+    /*
+    Descricao: Enum que lista todas as classes de token aceitas
+    */
     static enum TokenType {
-        Finaldaentrada,    Op_multiplicacao,    Op_divisao,     Op_mod,        Op_soma,     Op_subtracao,
-        Op_not,              Op_menor,      Op_menorigual, Op_maior,    Op_maiorigual,
-        Op_igual,          Op_naoigual,         Op_atribuicao, Op_and,        Op_or,       Palavrachave_if,
-        Palavrachave_else, Palavrachave_while,  Abparentese,   Fcparentese,   Abchave,     Fcchave,
-        Pontoevirgula,     Virgula,             Identificador, Inteiro,       String,      Tipodado,
-        Palavrachave_for,  Palavrachave_return, Palavrachave_include
+        Finaldaentrada,    Op_multiplicacao,    Op_divisao,     Op_mod,        Op_soma,         Op_subtracao,
+        Op_not,            Op_menor,            Op_menorigual,  Op_maior,      Op_maiorigual,   Op_igual,
+        Op_naoigual,       Op_atribuicao,       Op_and,         Op_or,         Palavrachave_if, Palavrachave_include,
+        Palavrachave_else, Palavrachave_while,  Abparentese,    Fcparentese,   Abchave,         Fcchave,
+        Pontoevirgula,     Virgula,             Identificador,  Inteiro,       String,          Tipodado,
+        Palavrachave_for,  Palavrachave_return
     }
  
-    static void error(int line, int pos, String msg) {
-        if (line > 0 && pos > 0) {
-            System.out.printf("%s na linha %d, coluna %d\n", msg, line, pos);
+    /*
+    Descricao: Função que imprime em qual linha e coluna ocorreu um erro e qual erro foi
+    Entrada: Linha e coluna do erro e mensagem de erro
+    Retorno: Nenhum
+    */
+    static void error(int linha, int pos, String msg) {
+        if (linha > 0 && pos > 0) {
+            System.out.printf("%s na linha %d, coluna %d\n", msg, linha, pos);
         } else {
             System.out.println(msg);
         }
-        System.exit(1);
     }
  
+    /*
+    Descricao: Cria uma instância da classe Lex com as informações do arquivo dado e identifica tokens de palavras chave
+    Entrada: String com todos os dados do arquivo
+    Retorno: Nenhum
+    */
     Lex (String source) {
-        this.line = 1;
+        this.linha = 1;
         this.pos = 0;
         this.position = 0;
         this.s = source;
@@ -75,21 +99,37 @@ public class Lex {
         this.keywords.put("float", TokenType.Tipodado);
 
     }
-    Token follow(char expect, TokenType ifyes, TokenType ifno, int line, int pos) {
+
+    /*
+    Descricao: Função que realiza a comparação de um caracter com um caractere esperado, criando novo token se não houver erros
+    Entrada: Caractere esperado, tipo de token caso o caractere seja o esperado, tipo de token caso o caractere não seja o esperado, linha e coluna do caractere
+    Retorno: Retorna o token criado
+    */
+    Token compara_caractere(char expect, TokenType ifyes, TokenType ifno, int linha, int pos) {
         if (getNextChar() == expect) {
             getNextChar();
-            return new Token(ifyes, "", line, pos);
+            return new Token(ifyes, "", linha, pos);
         }
         if (ifno == TokenType.Finaldaentrada) {
-            error(line, pos, String.format("caractere nao reconhecido: (%d) '%c'", (int)this.chr, this.chr));
+            error(linha, pos, String.format("ERRO: caractere nao reconhecido: (%d) '%c'", (int)this.chr, this.chr));
+            getNextChar();
+            return getToken();
         }
-        return new Token(ifno, "", line, pos);
+        return new Token(ifno, "", linha, pos);
     }
-    Token char_lit(int line, int pos) {
+
+    /*
+    Descricao: Função que realiza a leitura de um caractere e checa erros possíveis, criando novo token se não houver erros
+    Entrada: Linha e coluna do caractere
+    Retorno: Retorna o token criado
+    */
+    Token leitura_char(int linha, int pos) {
         char c = getNextChar(); // skip opening quote
         int n = (int)c;
         if (c == '\'') {
-            error(line, pos, "constante de caractere vazia");
+            error(linha, pos, "ERRO: constante de caractere vazia");
+            getNextChar();
+            return getToken();
         } else if (c == '\\') {
             c = getNextChar();
             if (c == 'n') {
@@ -97,37 +137,55 @@ public class Lex {
             } else if (c == '\\') {
                 n = '\\';
             } else {
-                error(line, pos, String.format("sequencia de escape nao reconhecida \\%c", c));
+                error(linha, pos, String.format("ERRO: sequencia de escape nao reconhecida \\%c", c));
+                getNextChar();
+                return getToken();
             }
         }
         if (getNextChar() != '\'') {
-            error(line, pos, "constante multi caracteres");
+            error(linha, pos, "ERRO: constante multi caracteres");
+            getNextChar();
+            return getToken();
         }
         getNextChar();
-        return new Token(TokenType.Inteiro, "" + n, line, pos);
+        return new Token(TokenType.Inteiro, "" + n, linha, pos);
     }
-    Token string_lit(char start, int line, int pos) {
+
+    /*
+    Descricao: Função que realiza a leitura de uma string e checa erros possíveis, criando novo token se não houver erros
+    Entrada: Caractere de começo da string, linha e coluna desse caractere
+    Retorno: Retorna o token criado
+    */
+    Token leitura_string(char start, int linha, int pos) {
         String result = "";
         while (getNextChar() != start) {
             if (this.chr == '\u0000') {
-                error(line, pos, "EOF enquanto lia string");
+                error(linha, pos, "ERRO: EOF enquanto lia string");
             }
             if (this.chr == '\n') {
-                error(line, pos, "EOL enquanto lia string");
+                error(linha, pos, "ERRO: EOL enquanto lia string");
+                getNextChar();
+                return getToken();
             }
             result += this.chr;
         }
         getNextChar();
-        return new Token(TokenType.String, result, line, pos);
+        return new Token(TokenType.String, result, linha, pos);
     }
-    Token div_or_comment(int line, int pos) {
+
+    /*
+    Descricao: Função que checa se o token é operador de divisão ou comentário e cria o token se for divisão
+    Entrada: Linha e posição do caractere
+    Retorno: Retorna o token criado se for divisão
+    */
+    Token div_ou_comment(int linha, int pos) {
         if (getNextChar() != '*') {
-            return new Token(TokenType.Op_divisao, "", line, pos);
+            return new Token(TokenType.Op_divisao, "", linha, pos);
         }
         getNextChar();
         while (true) { 
             if (this.chr == '\u0000') {
-                error(line, pos, "EOF em comentario");
+                error(linha, pos, "ERRO: EOF em comentario");
             } else if (this.chr == '*') {
                 if (getNextChar() == '/') {
                     getNextChar();
@@ -138,7 +196,13 @@ public class Lex {
             }
         }
     }
-    Token identifier_or_integer(int line, int pos) {
+
+    /*
+    Descricao: Função que checa se o token é inteiro ou identificador e cria o token com o tipo correto
+    Entrada: Linha e posição do caractere
+    Retorno: Retorna o token criado
+    */
+    Token identificador_ou_inteiro(int linha, int pos) {
         boolean is_number = true;
         String text = "";
  
@@ -151,54 +215,69 @@ public class Lex {
         }
  
         if (text.equals("")) {
-            error(line, pos, String.format("caractere de identificador ou inteiro não reconhecido: (%d) %c", (int)this.chr, this.chr));
+            error(linha, pos, String.format("ERRO: caractere nao reconhecido: (%d) %c", (int)this.chr, this.chr));
+            getNextChar();
+            return getToken();
         }
  
         if (Character.isDigit(text.charAt(0))) {
             if (!is_number) {
-                error(line, pos, String.format("número inválido: %s", text));
+                error(linha, pos, String.format("ERRO: numero invalido: %s", text));
+                getNextChar();
+                return getToken();
             }
-            return new Token(TokenType.Inteiro, text, line, pos);
+            return new Token(TokenType.Inteiro, text, linha, pos);
         }
  
         if (this.keywords.containsKey(text)) {
-            return new Token(this.keywords.get(text), "", line, pos);
+            return new Token(this.keywords.get(text), "", linha, pos);
         }
-        return new Token(TokenType.Identificador, text, line, pos);
+        return new Token(TokenType.Identificador, text, linha, pos);
     }
+
+    /*
+    Descricao: Função que cria o próximo token
+    Entrada: Nenhuma
+    Retorno: Retorna o token criado
+    */
     Token getToken() {
-        int line, pos;
+        int linha, pos;
         while (Character.isWhitespace(this.chr)) {
             getNextChar();
         }
-        line = this.line;
+        linha = this.linha;
         pos = this.pos;
  
         switch (this.chr) {
-            case '\u0000': return new Token(TokenType.Finaldaentrada, "", this.line, this.pos);
-            case '/': return div_or_comment(line, pos);
-            case '\'': return char_lit(line, pos);
-            case '<': return follow('=', TokenType.Op_menorigual, TokenType.Op_menor, line, pos);
-            case '>': return follow('=', TokenType.Op_maiorigual, TokenType.Op_maior, line, pos);
-            case '=': return follow('=', TokenType.Op_igual, TokenType.Op_atribuicao, line, pos);
-            case '!': return follow('=', TokenType.Op_naoigual, TokenType.Op_not, line, pos);
-            case '&': return follow('&', TokenType.Op_and, TokenType.Finaldaentrada, line, pos);
-            case '|': return follow('|', TokenType.Op_or, TokenType.Finaldaentrada, line, pos);
-            case '"': return string_lit(this.chr, line, pos);
-            case '{': getNextChar(); return new Token(TokenType.Abchave, "", line, pos);
-            case '}': getNextChar(); return new Token(TokenType.Fcchave, "", line, pos);
-            case '(': getNextChar(); return new Token(TokenType.Abparentese, "", line, pos);
-            case ')': getNextChar(); return new Token(TokenType.Fcparentese, "", line, pos);
-            case '+': getNextChar(); return new Token(TokenType.Op_soma, "", line, pos);
-            case '-': getNextChar(); return new Token(TokenType.Op_subtracao, "", line, pos);
-            case '*': getNextChar(); return new Token(TokenType.Op_multiplicacao, "", line, pos);
-            case '%': getNextChar(); return new Token(TokenType.Op_mod, "", line, pos);
-            case ';': getNextChar(); return new Token(TokenType.Pontoevirgula, "", line, pos);
-            case ',': getNextChar(); return new Token(TokenType.Virgula, "", line, pos);
-            default: return identifier_or_integer(line, pos);
+            case '\u0000': return new Token(TokenType.Finaldaentrada, "", this.linha, this.pos);
+            case '/': return div_ou_comment(linha, pos);
+            case '\'': return leitura_char(linha, pos);
+            case '<': return compara_caractere('=', TokenType.Op_menorigual, TokenType.Op_menor, linha, pos);
+            case '>': return compara_caractere('=', TokenType.Op_maiorigual, TokenType.Op_maior, linha, pos);
+            case '=': return compara_caractere('=', TokenType.Op_igual, TokenType.Op_atribuicao, linha, pos);
+            case '!': return compara_caractere('=', TokenType.Op_naoigual, TokenType.Op_not, linha, pos);
+            case '&': return compara_caractere('&', TokenType.Op_and, TokenType.Finaldaentrada, linha, pos);
+            case '|': return compara_caractere('|', TokenType.Op_or, TokenType.Finaldaentrada, linha, pos);
+            case '"': return leitura_string(this.chr, linha, pos);
+            case '{': getNextChar(); return new Token(TokenType.Abchave, "", linha, pos);
+            case '}': getNextChar(); return new Token(TokenType.Fcchave, "", linha, pos);
+            case '(': getNextChar(); return new Token(TokenType.Abparentese, "", linha, pos);
+            case ')': getNextChar(); return new Token(TokenType.Fcparentese, "", linha, pos);
+            case '+': getNextChar(); return new Token(TokenType.Op_soma, "", linha, pos);
+            case '-': getNextChar(); return new Token(TokenType.Op_subtracao, "", linha, pos);
+            case '*': getNextChar(); return new Token(TokenType.Op_multiplicacao, "", linha, pos);
+            case '%': getNextChar(); return new Token(TokenType.Op_mod, "", linha, pos);
+            case ';': getNextChar(); return new Token(TokenType.Pontoevirgula, "", linha, pos);
+            case ',': getNextChar(); return new Token(TokenType.Virgula, "", linha, pos);
+            default: return identificador_ou_inteiro(linha, pos);
         }
     }
  
+    /*
+    Descricao: Função que avança uma posição no arquivo e lê o próximo caractere, se houver
+    Entrada: Nenhuma
+    Retorno: Informações do próximo caractere do arquivo lido ou informaçaõ de finalizar a leitura
+    */
     char getNextChar() {
         this.pos++;
         this.position++;
@@ -208,12 +287,17 @@ public class Lex {
         }
         this.chr = this.s.charAt(this.position);
         if (this.chr == '\n') {
-            this.line++;
+            this.linha++;
             this.pos = 0;
         }
         return this.chr;
     }
  
+    /*
+    Descricao: Função que imprime a posição do token, seu tipo e se ele for identificador, inteiro ou string imprime seu valor
+    Entrada: Nenhuma
+    Retorno: Nenhum
+    */
     void printTokens() {
         Token t;
         while ((t = getToken()).tipotoken != TokenType.Finaldaentrada) {
@@ -221,6 +305,12 @@ public class Lex {
         }
         System.out.println(t);
     }
+
+    /*
+    Descricao: Função que solicita o nome do arquivo e apresenta o resultado da análise léxica
+    Entrada: Nenhuma
+    Retorno: Nenhum
+    */
     public static void main(String[] args) {
         String arquivo = null;
         System.out.println("Digite o nome do arquivo:");
@@ -229,7 +319,6 @@ public class Lex {
         System.out.println("Resultado da analise lexica:");
         if (arquivo != null) {
             try {
- 
                 File f = new File(arquivo);
                 Scanner s = new Scanner(f);
                 String source = " ";
@@ -241,8 +330,6 @@ public class Lex {
             } catch(FileNotFoundException e) {
                 error(-1, -1, "Exception: " + e.getMessage());
             }
-        } else {
-            error(-1, -1, "No args");
-        }
-    }
+        } 
+    }  
 }
